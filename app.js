6b51ac828e854e76c1ed5f0603136469d65925b6
Mauro Fassi <maurofassi17@gmail.com>
2023-02-20 15:39:03 -0300

    fin

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-//4:46:30
+const btns= document.querySelectorAll('.tab-btn')
+const about =document.querySelector('.about')
+const articles=document.querySelectorAll('.content')
+
+
+about.addEventListener('click',(e)=>{
+    //console.log(e.target.dataset.id)
+    const id=e.target.dataset.id
+
+    if(id){
+        //remove class active 
+        btns.forEach((btn)=>{
+            btn.classList.remove('active')
+
+            //add class active when a element clicked
+            e.target.classList.add('active')
+        })
+
+        //hide other articles 
+
+        articles.forEach((article)=>{
+            article.classList.remove('active')
+        })
+
+        const element=document.getElementById(id)
+        element.classList.add('active')
+
+    }
+
+})
+
+
+
+
+
+
+
+
+
